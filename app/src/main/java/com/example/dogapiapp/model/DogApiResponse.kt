34b285bfc0cf0aa3c4d4fcd94e34cfb61d0c88e6
Ademir <ademirespinozacuro@gmail.com>
2025-09-03 20:33:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dogapiapp/model/DogApiResponse.kt
@@ -0,0 +1,10 @@
+package com.example.dogapiapp.model
+
+import com.google.gson.annotations.SerializedName
+
+data class DogApiResponse(
+    @SerializedName("message")
+    val imageUrl: String,
+    val status: String
+)
+
